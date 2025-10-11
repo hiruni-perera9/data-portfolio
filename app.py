801ae147c2e3f663485d4ae2b5afaaa369dc372f
hiruni-perera9 <hiruni.perera.work@gmail.com>
2025-10-12 05:29:38 +1100

    Add Streamlit hello app

---
+++ app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.title("Hiruni Perera - Data Science Portfolio")
+st.write("Fast, explainable, business-first machine learning")
+st.success("Hello, recruiter. Everything ships on time")
